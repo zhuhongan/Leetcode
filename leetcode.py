7a5bd4b4cccca245f47b0844cc5065280a4de790
zhuhongan <61860772+zhuhongan@users.noreply.github.com>
2025-02-07 10:46:42 -0500

    Add files via upload

--- leetcode.py
+++ leetcode.py
@@ -1,1 +1,18 @@
+class Solution:
+    def searchInsert(self, nums, target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left+right) // 2
+            if nums[mid]<target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+    
+test_nums = [1,3,5,6]
+test_target = 5
 
+sol = Solution().searchInsert(test_nums, test_target)
+print(sol)
+
+        
